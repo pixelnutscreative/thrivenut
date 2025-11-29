f761dcc70edf920d2d99892f950b355a4087ac30
Pixel Nuts <pixelnutscreative@gmail.com>
2025-11-29 00:54:19 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -27,7 +27,6 @@ import Admin from './pages/Admin';
 import MeetThePixelNuts from './pages/MeetThePixelNuts';
 import PixelsParadise from './pages/PixelsParadise';
 import TikTokContacts from './pages/TikTokContacts';
-import Landing from './pages/Landing';
 import __Layout from './Layout.jsx';
 
 
@@ -61,7 +60,6 @@ export const PAGES = {
     "MeetThePixelNuts": MeetThePixelNuts,
     "PixelsParadise": PixelsParadise,
     "TikTokContacts": TikTokContacts,
-    "Landing": Landing,
 }
 
 export const pagesConfig = {
