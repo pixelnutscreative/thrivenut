d65ab107bbcf38a77d4003dfa2113ffc13b75b80
Pixel Nuts <pixelnutscreative@gmail.com>
2025-12-23 05:16:23 +0000

    File changes

--- src/pages/CreatorGroups.jsx
+++ src/pages/CreatorGroups.jsx
@@ -672,8 +672,9 @@ export default function CreatorGroups() {
           }}>Enter Invite Code</Button>
           <Button variant="outline" onClick={() => setSearchParams({})}>Back</Button>
         </div>
-        );
-        }
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen bg-gray-50/50">
