46461c3e09090fcff7e380117de7cafe337d567d
Pixel Nuts <pixelnutscreative@gmail.com>
2025-12-30 19:22:34 +0000

    File changes

--- src/components/groups/GroupSettingsTab.jsx
+++ src/components/groups/GroupSettingsTab.jsx
@@ -14,6 +14,7 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/component
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useTheme } from '@/components/shared/useTheme';
+import ColorPicker from '../shared/ColorPicker';
 
 export default function GroupSettingsTab({ group }) {
   return (
