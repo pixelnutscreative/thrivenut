35ddc5abf1777e195428015e3e5601f5fe82a826
Pixel Nuts <pixelnutscreative@gmail.com>
2026-01-03 04:16:25 +0000

    File changes

--- src/components/groups/GroupSettingsTab.jsx
+++ src/components/groups/GroupSettingsTab.jsx
@@ -427,6 +427,7 @@ function TabPermissionsSettings({ group }) {
     { id: 'meetings', label: 'Meetings' },
     { id: 'marketing', label: 'Marketing Orders' },
     { id: 'discussion', label: 'Discussion' },
+    { id: 'mod_schedule', label: 'Mod Schedule' },
     { id: 'members', label: 'Members' },
     { id: 'requests', label: 'Requests' }
   ];
